b5a002f5ae16615c6b7717d9842e1e83e1f0e241
secosun <sunxike@jhvege.com>
2021-08-22 22:33:10 +0800

    vscode init

--- jhvege/init.sh
+++ jhvege/init.sh
@@ -4,23 +4,23 @@ DOCKER_IMAGE_VERSION="v1"
 #docker pull ghcr.io/secosun/jhbase:latest || true
 
 # 使用最新拉取的镜像作为缓存层
-podman build \
+docker build \
   --cache-from ghcr.io/secosun/jhbase:latest \
   -t ghcr.io/secosun/jhbase:$DOCKER_IMAGE_VERSION \
   -t ghcr.io/secosun/jhbase:latest \
-  -f "jhvege/JHVEGEBASE/Dockerfile" \
-  "jhvege/JHVEGEBASE"
+  -f "JHVEGEBASE/Dockerfile" \
+  "JHVEGEBASE"
  
 
 # 拉取最新的镜像
 #docker pull ghcr.io/secosun/mysqlphpbase:latest || true
 # 使用最新拉取的镜像作为缓存层
-podman build \
+docker build \
   --cache-from ghcr.io/secosun/mysqlphpbase:latest \
   -t ghcr.io/secosun/mysqlphpbase:$DOCKER_IMAGE_VERSION \
   -t ghcr.io/secosun/mysqlphpbase:latest \
-  -f "jhvege/BASEPHPMYSQL/Dockerfile" \
-  "jhvege/BASEPHPMYSQL"
+  -f "BASEPHPMYSQL/Dockerfile" \
+  "BASEPHPMYSQL"
 
 # 重新推送  
 #docker push ghcr.io/secosun/jhbase:$DOCKER_IMAGE_VERSION
