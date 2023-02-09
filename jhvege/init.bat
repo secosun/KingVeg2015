5237694fdafe05b144f0c8e26abe8db120166a1c
sunxike <s.u.nxk@qq.com>
2023-02-10 00:35:40 +0800

    2023

--- jhvege/init.bat
+++ jhvege/init.bat
@@ -2,8 +2,8 @@ set DOCKER_IMAGE_VERSION="v1"
 set /p passwordcompose=<BASEPHPMYSQL\compose.pwd
 docker login ghcr.io -u secosun --password %passwordcompose%
 
-docker inspect --type=image  ghcr.io/secosun/jhvegebase:latest |  find /C "LastTagTime" >temp
-set /p existbase=<temp
+docker inspect --type=image  ghcr.io/secosun/jhvegebase:%DOCKER_IMAGE_VERSION% |  find /C "LastTagTime" >temp2
+set /p existbase=<temp2
 if %existbase%==1 goto A 
 
 :: 拉取最新的镜像
@@ -11,10 +11,9 @@ if %existbase%==1 goto A
 ::  --cache-from ghcr.io/secosun/jhbase:latest
 ::  --no-cache
 :: 使用最新拉取的镜像作为缓存层
-docker build ^
-  --cache-from ghcr.io/secosun/jhvegebase:latest ^
+::  --cache-from ghcr.io/secosun/jhvegebase:latest 
+docker build --no-cache ^
   -t ghcr.io/secosun/jhvegebase:%DOCKER_IMAGE_VERSION% ^
-  -t ghcr.io/secosun/jhvegebase:latest ^
   -f "JHVEGEBASE\Dockerfile" ^
   "JHVEGEBASE"
  
@@ -24,18 +23,17 @@ docker push ghcr.io/secosun/jhvegebase:%DOCKER_IMAGE_VERSION%
 docker push ghcr.io/secosun/jhvegebase:latest
 
 :A
-docker inspect --type=image  ghcr.io/secosun/mysqlphpbase:latest |  find /C "LastTagTime" >temp
-set /p existbase=<temp
+docker inspect --type=image  ghcr.io/secosun/mysqlphpbase:%DOCKER_IMAGE_VERSION% |  find /C "LastTagTime" >temp2
+set /p existbase=<temp2
 if %existbase%==1 goto B 
 :: 拉取最新的镜像
 ::::docker pull ghcr.io/secosun/mysqlphpbase:latest
 :: 使用最新拉取的镜像作为缓存层
 ::  --cache-from ghcr.io/secosun/mysqlphpbase:latest
 ::  --no-cache
-docker build ^
-  --cache-from ghcr.io/secosun/mysqlphpbase:latest ^
+::--cache-from ghcr.io/secosun/mysqlphpbase:%DOCKER_IMAGE_VERSION% 
+docker build --no-cache ^
   -t ghcr.io/secosun/mysqlphpbase:%DOCKER_IMAGE_VERSION% ^
-  -t ghcr.io/secosun/mysqlphpbase:latest ^
   -f "BASEPHPMYSQL/Dockerfile" ^
   "BASEPHPMYSQL"
 
@@ -43,5 +41,5 @@ docker build ^
 docker push ghcr.io/secosun/mysqlphpbase:%DOCKER_IMAGE_VERSION%
 docker push ghcr.io/secosun/mysqlphpbase:latest
 :B
-del  temp
+del  temp2
 echo init commpleted!
